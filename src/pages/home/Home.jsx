fd01dec65efe5d99aa410d82f89c62434371f9ef
Deb-das01 <97828828+Deb-das01@users.noreply.github.com>
2024-07-27 22:25:10 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -27,7 +27,7 @@ const Home = ({type}) => {
           {
             headers: {
               token:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTdmNjFjNTFjZmQ3NjNhYzc2OGMxMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcyMjA5ODgwMywiZXhwIjoxNzUzNjM0ODAzfQ.gxTBqew4WcmyLPT-S25cK0KqKBCZPPgPuELABPKOYJk",
+              "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTdmNjFjNTFjZmQ3NjNhYzc2OGMxMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcyMjA5ODgwMywiZXhwIjoxNzUzNjM0ODAzfQ.gxTBqew4WcmyLPT-S25cK0KqKBCZPPgPuELABPKOYJk",
             },
           }
         );
