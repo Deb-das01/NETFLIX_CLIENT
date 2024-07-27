0a01367cfb0bfb2a7c8ff94fe1f37016d80a4a19
Deb-das01 <97828828+Deb-das01@users.noreply.github.com>
2024-07-27 22:44:37 +0530

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -13,7 +13,7 @@ export default function Featured({ type,setGenre }) {
   useEffect(() => {
     const getRandomContent = async () => {
       try {
-        const res = await axios.get(`/movies/random?type=${type}`, {
+        const res = await axios.get(`https://netflix-mern-backend.onrender.com/api/movies/random?type=${type}`, {
           headers: {
             token:
              "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
